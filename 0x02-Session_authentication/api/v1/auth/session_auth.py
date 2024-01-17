81e284d3b8da8f4db3a4fd0e10f2b9e290198a67
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-17 20:58:28 +0100

    added session auth current user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,8 @@
 
 from uuid import uuid4
 from api.v1.auth.auth import Auth
+from models.user import User
+from models.base import DATA
 
 
 class SessionAuth(Auth):
@@ -23,3 +25,13 @@ class SessionAuth(Auth):
         if not session_id or not isinstance(session_id, str):
             return None
         return type(self).user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns user based on given cookie"""
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+        User.load_from_file()
+        if 'User' not in DATA:
+            return None
+        user = User.get(user_id)
+        return user
