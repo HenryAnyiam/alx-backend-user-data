194d141c27318c27cf144dfee9a3e507422c1ea1
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-23 16:33:31 +0100

    fixed pep8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,7 +33,8 @@ class Auth:
             user = self._db.add_user(email=email,
                                      hashed_password=hashedpwd)
             return user
-    def valid_login(self, email:str, password:str) -> bool:
+
+    def valid_login(self, email: str, password: str) -> bool:
         """validate user login"""
         try:
             user = self._db.find_user_by(email=email)
