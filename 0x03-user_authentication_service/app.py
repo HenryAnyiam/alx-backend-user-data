403278a84acb88ee61b8db7843f2268792e70310
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-23 17:17:57 +0100

    added login functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,6 +2,7 @@
 """create app"""
 
 from flask import Flask, jsonify, request
+from flask import abort, make_response
 from auth import Auth
 
 
@@ -29,6 +30,22 @@ def users():
         res = jsonify({"email": f"{email}",
                        "message": "user created"})
         return res, 200
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """login user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user = AUTH.valid_login(email, password)
+    if user:
+        session_id = AUTH.create_session(email)
+        res = make_response({"email": f"{email}",
+                             "message": "logged in"})
+        res.set_cookie("session_id", session_id)
+        return res
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
