cdfaae840022c786d1195d663fec8be283ca9180
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-23 10:12:08 +0100

    removed sequence from id

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """Map a class to table User"""
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
@@ -13,8 +13,7 @@ class User(Base):
 
     __tablename__ = "users"
 
-    id = Column(Integer, Sequence('user_id_seq'),
-                primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
