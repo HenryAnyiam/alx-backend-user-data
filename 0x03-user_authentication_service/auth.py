0efc0da52d03e2539b0a59155f7122a93ba40d39
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-24 09:32:47 +0100

    added password reset functionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,12 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update user password"""
+        user = self._db.find_user_by(reset_token=reset_token)
+        if not user:
+            raise ValueError
+        hashedpwd = _hash_password(password)
+        self._db.update_user(user.id, reset_token=None,
+                             hashed_password=hashedpwd)
